99411f91551e637fd9167f9a5861c35db5b94cf5
Razria-Notebook <dsyeom931008@gmail.com>
2026-07-08 16:41:15 +0900

    chore: Part1 DX11 초기화 예제 코드 추가

---
+++ Part1_Chapter01-02/01_DX11InitAndImGui/main.cpp
@@ -0,0 +1,139 @@
+﻿#include <imgui.h>
+#include <imgui_impl_dx11.h>
+#include <imgui_impl_win32.h>
+#include <windows.h>
+#include <memory>
+#include <iostream>
+
+#include "Example.h"
+
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int main()
+{
+	const int width = 1280, height = 960;
+	const int canvasWidth = width / 80, canvasHeight = height / 80;
+
+	// Win32 window setup.
+	WNDCLASSEX wc = {
+		sizeof(WNDCLASSEX),
+		CS_CLASSDC,
+		WndProc,
+		0L,
+		0L,
+		GetModuleHandle(NULL),
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		L"HongLabGraphics",
+		NULL
+	};
+
+	RegisterClassEx(&wc);
+
+	RECT wr = { 0, 0, width, height };
+	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
+
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,
+		L"HongLabGraphics Example",
+		WS_OVERLAPPEDWINDOW,
+		100,
+		100,
+		wr.right - wr.left,
+		wr.bottom - wr.top,
+		NULL,
+		NULL,
+		wc.hInstance,
+		NULL);
+
+	ShowWindow(hwnd, SW_SHOWDEFAULT);
+	UpdateWindow(hwnd);
+
+	auto example = std::make_unique<Example>(hwnd, width, height, canvasWidth, canvasHeight);
+
+	// ImGui overlay setup.
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.DisplaySize = ImVec2(width, height);
+	ImGui::StyleColorsLight();
+
+	ImGui_ImplDX11_Init(example->device, example->deviceContext);
+	ImGui_ImplWin32_Init(hwnd);
+
+	// Win32 message loop and per-frame render path.
+	MSG msg = {};
+	while (WM_QUIT != msg.message)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			ImGui_ImplDX11_NewFrame();
+			ImGui_ImplWin32_NewFrame();
+			ImGui::NewFrame();
+
+			ImGui::Begin("Background Color");
+
+			ImGui::SliderFloat3("RGB(0.0->1.0)", example->backgroundColor, 0.0f, 1.0f);
+
+			ImGui::End();
+			ImGui::Render();
+
+			std::cout << "canvasColor" << example->backgroundColor[0] << " " << example->backgroundColor[1] << " " << example->backgroundColor[2] << " " << example->backgroundColor[3] << std::endl;
+
+			example->Update();
+			example->Render();
+
+			ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+
+			example->swapChain->Present(1, 0);
+		}
+	}
+
+	ImGui_ImplDX11_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+
+	example->Clean();
+	DestroyWindow(hwnd);
+	UnregisterClass(wc.lpszClassName, wc.hInstance);
+
+	return 0;
+}
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+		return true;
+
+	switch (msg)
+	{
+	case WM_SIZE:
+		return 0;
+	case WM_SYSCOMMAND:
+		if ((wParam & 0xfff0) == SC_KEYMENU)
+			return 0;
+		break;
+	case WM_MOUSEMOVE:
+		break;
+	case WM_LBUTTONUP:
+		break;
+	case WM_RBUTTONUP:
+		break;
+	case WM_KEYDOWN:
+		break;
+	case WM_DESTROY:
+		::PostQuitMessage(0);
+		return 0;
+	}
+
+	return ::DefWindowProc(hWnd, msg, wParam, lParam);
+}
