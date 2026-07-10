1fa40134ee1ddb0ee6efa32a5e8d9c1899d134bc
Razria-Notebook <dsyeom931008@gmail.com>
2026-07-11 02:01:23 +0900

    docs: Part2 Cubemapping 예제 import

---
+++ Part2_Chapter05-08/08_ShaderToys_Step2_Cubemapping/CubeMapping.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <wrl.h>
+
+#include "GeometryGenerator.h"
+#include "Material.h"
+#include "Vertex.h"
+
+namespace hlab {
+
+using Microsoft::WRL::ComPtr;
+
+struct CubeMapping {
+
+    std::shared_ptr<Mesh> cubeMesh;                         // Sphere도 사용가능 -> 만들어보기
+
+    ComPtr<ID3D11ShaderResourceView> cubemapResourceView;   // 텍스처를 Resource로 쓸 수 있게 해주는 View객체
+
+    ComPtr<ID3D11VertexShader> vertexShader;                // 큐브 매핑할 때 사용하는 쉐이더들은 다름. 조금 더 단순함
+    ComPtr<ID3D11PixelShader> pixelShader;
+    ComPtr<ID3D11InputLayout> inputLayout;
+};
+} // namespace hlab
