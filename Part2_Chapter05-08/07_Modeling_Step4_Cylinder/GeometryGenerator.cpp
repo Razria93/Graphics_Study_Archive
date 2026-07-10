317b9e7994e1bd8d0beac0e9fe2e2171928aad0e
Razria-Notebook <dsyeom931008@gmail.com>
2026-07-10 11:46:00 +0900

    docs: Part2 Cylinder 예제 import

---
+++ Part2_Chapter05-08/07_Modeling_Step4_Cylinder/GeometryGenerator.cpp
@@ -0,0 +1,298 @@
+﻿#include "GeometryGenerator.h"
+
+#include <iostream>
+
+namespace hlab
+{
+
+using namespace std;
+
+MeshData GeometryGenerator::MakeSquare()
+{
+	vector<Vector3> positions;
+	vector<Vector3> colors;
+	vector<Vector3> normals;
+	vector<Vector2> texcoords;
+
+	const float scale = 1.0f;
+
+
+	positions.push_back(Vector3(-1.0f, 1.0f, 0.0f) * scale);
+	positions.push_back(Vector3(1.0f, 1.0f, 0.0f) * scale);
+	positions.push_back(Vector3(1.0f, -1.0f, 0.0f) * scale);
+	positions.push_back(Vector3(-1.0f, -1.0f, 0.0f) * scale);
+	colors.push_back(Vector3(0.0f, 0.0f, 1.0f));
+	colors.push_back(Vector3(0.0f, 0.0f, 1.0f));
+	colors.push_back(Vector3(0.0f, 0.0f, 1.0f));
+	colors.push_back(Vector3(0.0f, 0.0f, 1.0f));
+	normals.push_back(Vector3(0.0f, 0.0f, -1.0f));
+	normals.push_back(Vector3(0.0f, 0.0f, -1.0f));
+	normals.push_back(Vector3(0.0f, 0.0f, -1.0f));
+	normals.push_back(Vector3(0.0f, 0.0f, -1.0f));
+
+
+
+	texcoords.push_back(Vector2(0.0f, 0.0f));
+	texcoords.push_back(Vector2(1.0f, 0.0f));
+	texcoords.push_back(Vector2(1.0f, 1.0f));
+	texcoords.push_back(Vector2(0.0f, 1.0f));
+
+	MeshData meshData;
+
+	for (size_t i = 0; i < positions.size(); i++)
+	{
+		Vertex v;
+		v.position = positions[i];
+		v.normal = normals[i];
+		v.texcoord = texcoords[i];
+
+
+
+		meshData.vertices.push_back(v);
+	}
+	meshData.indices = {
+	    0, 1, 2, 0, 2, 3,
+	};
+
+	return meshData;
+}
+
+MeshData GeometryGenerator::MakeBox()
+{
+
+	vector<Vector3> positions;
+	vector<Vector3> colors;
+	vector<Vector3> normals;
+	vector<Vector2> texcoords;
+
+	const float scale = 1.0f;
+
+
+	positions.push_back(Vector3(-1.0f, 1.0f, -1.0f) * scale);
+	positions.push_back(Vector3(-1.0f, 1.0f, 1.0f) * scale);
+	positions.push_back(Vector3(1.0f, 1.0f, 1.0f) * scale);
+	positions.push_back(Vector3(1.0f, 1.0f, -1.0f) * scale);
+	colors.push_back(Vector3(1.0f, 0.0f, 0.0f));
+	colors.push_back(Vector3(1.0f, 0.0f, 0.0f));
+	colors.push_back(Vector3(1.0f, 0.0f, 0.0f));
+	colors.push_back(Vector3(1.0f, 0.0f, 0.0f));
+	normals.push_back(Vector3(0.0f, 1.0f, 0.0f));
+	normals.push_back(Vector3(0.0f, 1.0f, 0.0f));
+	normals.push_back(Vector3(0.0f, 1.0f, 0.0f));
+	normals.push_back(Vector3(0.0f, 1.0f, 0.0f));
+	texcoords.push_back(Vector2(0.0f, 0.0f));
+	texcoords.push_back(Vector2(1.0f, 0.0f));
+	texcoords.push_back(Vector2(1.0f, 1.0f));
+	texcoords.push_back(Vector2(0.0f, 1.0f));
+
+
+	positions.push_back(Vector3(-1.0f, -1.0f, -1.0f) * scale);
+	positions.push_back(Vector3(1.0f, -1.0f, -1.0f) * scale);
+	positions.push_back(Vector3(1.0f, -1.0f, 1.0f) * scale);
+	positions.push_back(Vector3(-1.0f, -1.0f, 1.0f) * scale);
+	colors.push_back(Vector3(0.0f, 1.0f, 0.0f));
+	colors.push_back(Vector3(0.0f, 1.0f, 0.0f));
+	colors.push_back(Vector3(0.0f, 1.0f, 0.0f));
+	colors.push_back(Vector3(0.0f, 1.0f, 0.0f));
+	normals.push_back(Vector3(0.0f, -1.0f, 0.0f));
+	normals.push_back(Vector3(0.0f, -1.0f, 0.0f));
+	normals.push_back(Vector3(0.0f, -1.0f, 0.0f));
+	normals.push_back(Vector3(0.0f, -1.0f, 0.0f));
+	texcoords.push_back(Vector2(0.0f, 0.0f));
+	texcoords.push_back(Vector2(1.0f, 0.0f));
+	texcoords.push_back(Vector2(1.0f, 1.0f));
+	texcoords.push_back(Vector2(0.0f, 1.0f));
+
+
+	positions.push_back(Vector3(-1.0f, -1.0f, -1.0f) * scale);
+	positions.push_back(Vector3(-1.0f, 1.0f, -1.0f) * scale);
+	positions.push_back(Vector3(1.0f, 1.0f, -1.0f) * scale);
+	positions.push_back(Vector3(1.0f, -1.0f, -1.0f) * scale);
+	colors.push_back(Vector3(0.0f, 0.0f, 1.0f));
+	colors.push_back(Vector3(0.0f, 0.0f, 1.0f));
+	colors.push_back(Vector3(0.0f, 0.0f, 1.0f));
+	colors.push_back(Vector3(0.0f, 0.0f, 1.0f));
+	normals.push_back(Vector3(0.0f, 0.0f, -1.0f));
+	normals.push_back(Vector3(0.0f, 0.0f, -1.0f));
+	normals.push_back(Vector3(0.0f, 0.0f, -1.0f));
+	normals.push_back(Vector3(0.0f, 0.0f, -1.0f));
+	texcoords.push_back(Vector2(0.0f, 0.0f));
+	texcoords.push_back(Vector2(1.0f, 0.0f));
+	texcoords.push_back(Vector2(1.0f, 1.0f));
+	texcoords.push_back(Vector2(0.0f, 1.0f));
+
+
+	positions.push_back(Vector3(-1.0f, -1.0f, 1.0f) * scale);
+	positions.push_back(Vector3(1.0f, -1.0f, 1.0f) * scale);
+	positions.push_back(Vector3(1.0f, 1.0f, 1.0f) * scale);
+	positions.push_back(Vector3(-1.0f, 1.0f, 1.0f) * scale);
+	colors.push_back(Vector3(0.0f, 1.0f, 1.0f));
+	colors.push_back(Vector3(0.0f, 1.0f, 1.0f));
+	colors.push_back(Vector3(0.0f, 1.0f, 1.0f));
+	colors.push_back(Vector3(0.0f, 1.0f, 1.0f));
+	normals.push_back(Vector3(0.0f, 0.0f, 1.0f));
+	normals.push_back(Vector3(0.0f, 0.0f, 1.0f));
+	normals.push_back(Vector3(0.0f, 0.0f, 1.0f));
+	normals.push_back(Vector3(0.0f, 0.0f, 1.0f));
+	texcoords.push_back(Vector2(0.0f, 0.0f));
+	texcoords.push_back(Vector2(1.0f, 0.0f));
+	texcoords.push_back(Vector2(1.0f, 1.0f));
+	texcoords.push_back(Vector2(0.0f, 1.0f));
+
+
+	positions.push_back(Vector3(-1.0f, -1.0f, 1.0f) * scale);
+	positions.push_back(Vector3(-1.0f, 1.0f, 1.0f) * scale);
+	positions.push_back(Vector3(-1.0f, 1.0f, -1.0f) * scale);
+	positions.push_back(Vector3(-1.0f, -1.0f, -1.0f) * scale);
+	colors.push_back(Vector3(1.0f, 1.0f, 0.0f));
+	colors.push_back(Vector3(1.0f, 1.0f, 0.0f));
+	colors.push_back(Vector3(1.0f, 1.0f, 0.0f));
+	colors.push_back(Vector3(1.0f, 1.0f, 0.0f));
+	normals.push_back(Vector3(-1.0f, 0.0f, 0.0f));
+	normals.push_back(Vector3(-1.0f, 0.0f, 0.0f));
+	normals.push_back(Vector3(-1.0f, 0.0f, 0.0f));
+	normals.push_back(Vector3(-1.0f, 0.0f, 0.0f));
+	texcoords.push_back(Vector2(0.0f, 0.0f));
+	texcoords.push_back(Vector2(1.0f, 0.0f));
+	texcoords.push_back(Vector2(1.0f, 1.0f));
+	texcoords.push_back(Vector2(0.0f, 1.0f));
+
+
+	positions.push_back(Vector3(1.0f, -1.0f, 1.0f) * scale);
+	positions.push_back(Vector3(1.0f, -1.0f, -1.0f) * scale);
+	positions.push_back(Vector3(1.0f, 1.0f, -1.0f) * scale);
+	positions.push_back(Vector3(1.0f, 1.0f, 1.0f) * scale);
+	colors.push_back(Vector3(1.0f, 0.0f, 1.0f));
+	colors.push_back(Vector3(1.0f, 0.0f, 1.0f));
+	colors.push_back(Vector3(1.0f, 0.0f, 1.0f));
+	colors.push_back(Vector3(1.0f, 0.0f, 1.0f));
+	normals.push_back(Vector3(1.0f, 0.0f, 0.0f));
+	normals.push_back(Vector3(1.0f, 0.0f, 0.0f));
+	normals.push_back(Vector3(1.0f, 0.0f, 0.0f));
+	normals.push_back(Vector3(1.0f, 0.0f, 0.0f));
+	texcoords.push_back(Vector2(0.0f, 0.0f));
+	texcoords.push_back(Vector2(1.0f, 0.0f));
+	texcoords.push_back(Vector2(1.0f, 1.0f));
+	texcoords.push_back(Vector2(0.0f, 1.0f));
+
+	MeshData meshData;
+	for (size_t i = 0; i < positions.size(); i++)
+	{
+		Vertex v;
+		v.position = positions[i];
+		v.normal = normals[i];
+		v.texcoord = texcoords[i];
+		meshData.vertices.push_back(v);
+	}
+
+	meshData.indices = {
+	    0, 1, 2, 0, 2, 3,
+	    4, 5, 6, 4, 6, 7,
+	    8, 9, 10, 8, 10, 11,
+	    12, 13, 14, 12, 14, 15,
+	    16, 17, 18, 16, 18, 19,
+	    20, 21, 22, 20, 22, 23
+	};
+
+	return meshData;
+}
+
+MeshData GeometryGenerator::MakeCylinder(const float bottomRadius,
+                                         const float topRadius, float height,
+                                         int sliceCount)
+{
+
+	using namespace DirectX;
+	using DirectX::SimpleMath::Matrix;
+	using DirectX::SimpleMath::Vector3;
+
+
+
+	const float dTheta = XM_2PI / float(sliceCount);
+
+	MeshData meshData;
+
+	Vector3 center = Vector3(0.0f);
+
+	vector<Vertex> &vertices = meshData.vertices;
+
+
+	for (int i = 0; i <= sliceCount; i++)
+	{
+		Vertex v;
+
+		Vector3 v0 = Vector3(abs(bottomRadius), 0.0f, 0.0f);
+		Vector3 normal = Vector3(1.0f, 0.0f, 0.0f);
+
+		Matrix rotate = Matrix::CreateRotationY(dTheta * float(i));
+		Matrix rotate_invpos = rotate.Invert().Transpose();
+		
+		v0 = Vector3::Transform(v0, rotate);
+
+		normal = Vector3::Transform(normal, rotate_invpos);
+
+		v.position = v0;
+		v.normal = normal;
+
+		v.texcoord.x = float(i) / sliceCount;
+		v.texcoord.y = 1.0f;
+
+		vertices.push_back(v);
+	}
+
+	Matrix move_height = Matrix::CreateTranslation(0.0f, height, 0.0f);
+	center = Vector3::Transform(center, move_height);
+
+
+	for (int i = 0; i <= sliceCount; i++)
+	{
+		Vertex v;
+
+		Vector3 v0 = Vector3(abs(topRadius), 0.0f, 0.0f);
+		Vector3 normal = Vector3(1.0f, 0.0f, 0.0f);
+
+		Matrix rotate = Matrix::CreateRotationY(dTheta * float(i));
+		Matrix rotate_invpos = rotate.Invert().Transpose();
+
+		Matrix translate = Matrix::CreateTranslation(center);
+
+		v0 = Vector3::Transform(v0, rotate);
+		v0 = Vector3::Transform(v0, translate);
+
+		normal = Vector3::Transform(normal, rotate_invpos);
+
+		v.position = v0;
+		v.normal = normal;
+
+		v.texcoord.x = float(i) / sliceCount;
+		v.texcoord.y = 0.0f;
+
+		vertices.push_back(v);
+	}
+
+	vector<uint16_t> &indices = meshData.indices;
+
+	int offset = sliceCount + 1;
+
+	for (int i = 0; i < sliceCount; i++)
+	{
+		int i0 = i;
+		int i1 = i + 1;
+		int i2 = i + offset;
+		int i3 = i + 1 + offset;
+
+		indices.push_back(i0);
+		indices.push_back(i2);
+		indices.push_back(i3);
+
+		indices.push_back(i0);
+		indices.push_back(i3);
+		indices.push_back(i1);
+	}
+
+	meshData.vertices = vertices;
+	meshData.indices = indices;
+
+	return meshData;
+}
+}
