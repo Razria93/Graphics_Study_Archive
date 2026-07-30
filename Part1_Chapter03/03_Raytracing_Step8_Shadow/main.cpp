7ad2d740d4a7ac8f12e3212a964df2bdb74f9728
Razria-Notebook <dsyeom931008@gmail.com>
2026-07-31 00:44:53 +0900

    build: Chapter03 Step8 build run 검증

--- Part1_Chapter03/03_Raytracing_Step8_Shadow/main.cpp
+++ Part1_Chapter03/03_Raytracing_Step8_Shadow/main.cpp
@@ -37,7 +37,7 @@ int main()
 
     HWND hwnd = CreateWindow(
         wc.lpszClassName,
-        L"HongLabGraphics Example",
+        L"ComputerGraphics - Step8 Shadow",
         WS_OVERLAPPEDWINDOW,
         100,
         100,
