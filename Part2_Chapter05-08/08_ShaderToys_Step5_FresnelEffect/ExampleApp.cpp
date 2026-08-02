1c87ae6de17938247c978bdbb6f5ca0b4c954b96
Razria-Notebook <dsyeom931008@gmail.com>
2026-08-03 02:29:58 +0900

    fix: Chapter08 Step5 FresnelEffect 실행 상태 정리

--- Part2_Chapter05-08/08_ShaderToys_Step5_FresnelEffect/ExampleApp.cpp
+++ Part2_Chapter05-08/08_ShaderToys_Step5_FresnelEffect/ExampleApp.cpp
@@ -15,7 +15,7 @@ ExampleApp::ExampleApp() : AppBase(), m_BasicPixelConstantBufferData() {}
 
 // 코드 구조가 조금씩 복잡해지고 있습니다.
 // 일단은 이해하기 단순하게 구현해봅시다.
-void ExampleApp::InitializeCubeMapping() {
+bool ExampleApp::InitializeCubeMapping() {
 
     // texassemble.exe cube -w 2048 -h 2048 -o saintpeters.dds posx.jpg negx.jpg
     // posy.jpg negy.jpg posz.jpg negz.jpg
@@ -33,10 +33,11 @@ void ExampleApp::InitializeCubeMapping() {
         L"./CubemapTextures/Stonewall_diffuseIBL.dds";
 
     // .dds 파일 읽어들여서 초기화
-    CreateCubemapTexture(stonewallDiffuseFilename,
-                         m_cubeMapping.diffuseResView);
-    CreateCubemapTexture(stonewallSpecularFilename,
-                         m_cubeMapping.specularResView);
+    if (!CreateCubemapTexture(stonewallDiffuseFilename,
+                              m_cubeMapping.diffuseResView) ||
+        !CreateCubemapTexture(stonewallSpecularFilename,
+                              m_cubeMapping.specularResView))
+        return false;
 
     m_cubeMapping.cubeMesh = std::make_shared<Mesh>();
 
@@ -85,6 +86,7 @@ void ExampleApp::InitializeCubeMapping() {
 
     // 기타
     // - 텍스춰 샘플러도 다른 텍스춰와 같이 사용
+    return true;
 }
 
 bool ExampleApp::Initialize() {
@@ -93,7 +95,8 @@ bool ExampleApp::Initialize() {
         return false;
 
     // 큐브매핑 준비
-    InitializeCubeMapping();
+    if (!InitializeCubeMapping())
+        return false;
 
     // Texture sampler 만들기
     D3D11_SAMPLER_DESC sampDesc;
@@ -107,7 +110,9 @@ bool ExampleApp::Initialize() {
     sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
 
     // Create the Sample State
-    m_device->CreateSamplerState(&sampDesc, m_samplerState.GetAddressOf());
+    if (FAILED(m_device->CreateSamplerState(&sampDesc,
+                                            m_samplerState.GetAddressOf())))
+        return false;
 
     // Geometry 정의
 
